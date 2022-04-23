f3aad009e72ddbbda3c116aa42787cbbe4b1f30f
0uz <39968410+0uz@users.noreply.github.com>
2022-04-23 16:17:28 +0000

    Deploying to gh-pages from @ 0uz/furniture-website@5185f6191df2d9f4941f85ce62e02e55d6999416 🚀

---
+++ _next/static/lulcu6aQa-PX_qgnU2sGa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-dfddf3711b0bb668.js","static/chunks/pages/index-4cdf498e089cdbcd.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
